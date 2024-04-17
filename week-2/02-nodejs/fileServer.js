df65e2b5c2c0651b6d2ef51749561333742032f5
Prasad Karmalkar <prasadkarmalkar2020@gmail.com>
2024-04-17 15:09:20 +0530

    Create a todo server and file access server

--- week-2/02-nodejs/fileServer.js
+++ week-2/02-nodejs/fileServer.js
@@ -3,19 +3,42 @@
   - Use built in Node.js `fs` module
   The expected API endpoints are defined below,
   1. GET /files - Returns a list of files present in `./files/` directory
-    Response: 200 OK with an array of file names in JSON format.
-    Example: GET http://localhost:3000/files
+	Response: 200 OK with an array of file names in JSON format.
+	Example: GET http://localhost:3000/files
   2. GET /file/:filename - Returns content of given file by name
-     Description: Use the filename from the request path parameter to read the file from `./files/` directory
-     Response: 200 OK with the file content as the response body if found, or 404 Not Found if not found. Should return `File not found` as text if file is not found
-     Example: GET http://localhost:3000/file/example.txt
-    - For any other route not defined in the server return 404
-    Testing the server - run `npm run test-fileServer` command in terminal
+	 Description: Use the filename from the request path parameter to read the file from `./files/` directory
+	 Response: 200 OK with the file content as the response body if found, or 404 Not Found if not found. Should return `File not found` as text if file is not found
+	 Example: GET http://localhost:3000/file/example.txt
+	- For any other route not defined in the server return 404
+	Testing the server - run `npm run test-fileServer` command in terminal
  */
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
+app.get( '/files', ( req, res ) => {
+	fs.readdir( './files/', 'utf-8', ( err, files ) =>{
+		if( err ) {
+			res.status(500).json({ error: 'Failed to retrieve files' });
+		}
+		res.status(200).json( files );
+	} )
+	
+} )
 
+app.get( '/file/:filename', async ( req, res ) => {
+		fs.readFile( './files/' + req.params.filename, 'utf8', (err, data) =>{
+			if (err) {
+				console.log(err)
+				res.status(404).send('File not found');
+			} else {
+				res.status(200).send(data);
+			}
+		} );
+} )
 
+app.get('*', (req,res)=>{
+	res.status(404).send("Route not found");
+})
+app.listen(3000)
 module.exports = app;
